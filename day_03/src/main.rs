eb1c4e4f5ec3574cdd268b38920c5afec26ae481
Mathan Geurtsen <MGeurtsen@infiniot.nl>
2023-12-06 18:20:37 +0100

    day3 boilerplate

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::i32;
+use std::io::{self, BufRead};
+use regex::Regex;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> 
+{
+    let file = File::open("../advent_of_code_secrets/2023/day_03/input.txt")?;
+    let reader = io::BufReader::new(file);
+   
+    let mut possible: Vec<i32> = Vec::new();
+    let mut powers = Vec::<i32>::new();
+    
+    for line_result in reader.lines() {
+        let Ok(line) = line_result
+        else { return Err("Failed to parse game nr".into()) };
+    }
+    Ok(())
 }
