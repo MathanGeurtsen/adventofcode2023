9fba10c4840656e266044827b80614bd7ab5bf18
mathan geurtsen <me@mathangeurtsen.nl>
2023-12-06 01:53:40 +0100

    finished day 2 part 1

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -3,16 +3,6 @@ use std::i32;
 use std::io::{self, BufRead};
 use regex::Regex;
 
-#[derive(Debug)]
-#[derive(Default)]
-struct Game {
-    game_nr: i32,
-    nr_red: i32,
-    nr_green: i32,
-    nr_blue: i32,
-    line: String
-}
-
 fn main() -> Result<(), Box<dyn std::error::Error>> 
 {
     let file = File::open("../advent_of_code_secrets/2023/day_02/input.txt")?;
@@ -22,29 +12,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>>
     let green_max = 13;
     let blue_max = 14;
     
-    let mut games: Vec<Game> = Vec::new();
+    let mut possible: Vec<i32> = Vec::new();
     
     // Game 1: 1 red, 5 blue, 1 green; 16 blue, 3 red; 6 blue, 5 red; 4 red, 7 blue, 1 green
     
     for line_result in reader.lines() {
-       
-        let split_line : Vec<String>= line_result?.split(':').map(|x| x.to_string()).collect();
-
-        let mut game = Game {
-            game_nr: 0,
-            nr_red: 0,
-            nr_green: 0,
-            nr_blue: 0,
-            line: split_line[1].clone()
-        };
-
-
+        let Ok(line) = line_result
+        else { return Err("Failed to parse game nr".into()) };
+        
+        let split_line : Vec<String>= line.split(':').map(|x| x.to_string()).collect();
+        
         let Some(game_nr_cap) = Regex::new("Game (?<nr>[0-9]+)")?.captures(&split_line[0])
         else { return Err("Failed to parse game nr".into()) };
-        game.game_nr = game_nr_cap["nr"].parse()?;
+        let game_nr = game_nr_cap["nr"].parse()?;
         
+        let mut poss: bool = true;
+       
         let hands: Vec<Vec<&str>> = split_line[1].split(';').map(|hand| hand.split(',').collect()).collect();
         for hand in hands{
+            let mut nr_red = 0;
+            let mut nr_green = 0;
+            let mut nr_blue = 0;
+
             for group in hand {
                 let Some(split_group) = Regex::new("(?<nr>[0-9]+) (?<color>[a-z]+)")?.captures(group)
                 else { return Err("failed to parse group".into()) };
@@ -52,26 +41,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>>
                 let color = &split_group["color"];
                 
                 match color {
-                    "red" => { game.nr_red += nr},
-                    "green" => { game.nr_green += nr},
-                    "blue" => { game.nr_blue += nr},
+                    "red" => { nr_red += nr},
+                    "green" => { nr_green += nr},
+                    "blue" => { nr_blue += nr},
                     _ => {return Err("failed to split with ':'".into()) }
                 } 
             }
-        }
-        games.push(game)
-    }
-    let mut possible: Vec<i32> = Vec::new();
-    
-    for game in games {
-        let mut poss: bool = false;
-        if game.nr_red <= red_max && game.nr_green <= green_max && game.nr_blue <= blue_max {
-            poss = true;
-            possible.push(game.game_nr)
-        }
 
-        println!("{:#?}, {:#?}", game, poss);
+            poss = (nr_red <= red_max && nr_green <= green_max && nr_blue <= blue_max) && poss;
+        }
+        if poss{
+            possible.push(game_nr)
+        }
+        
+        println!("{:?}, {:?}", line, poss)
     }
-    println!("{:?}", possible.iter().sum::<i32>());
+    let i: i32 =    possible.iter().sum();
+    println!("{:#?}, {:?}", possible, i);
+    
     Ok(())
 }
